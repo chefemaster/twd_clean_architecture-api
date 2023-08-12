2bacc02bdb2dbb5b6f7b360989359209e3e6162a
Jeferson M Bruno <jeferson.m.bruno@gmail.com>
2023-08-12 20:04:22 -0300

    feat: add and find user and check its existence

--- src/usecases/register_user_on_mailing_list/repository/in_memory_user_repository.spec.ts
+++ src/usecases/register_user_on_mailing_list/repository/in_memory_user_repository.spec.ts
@@ -8,4 +8,24 @@ describe('In memory User repository', () => {
     const user = await userRepo.findUserByEmail('any@mail.com');
     expect(user).toBeNull();
   })
+
+  test('should return exists record if add user', async () => {
+    const users: UserData[] = [];
+    const name = 'any_name';
+    const email = 'any@gmail.com';
+    const userRepo = new InMemoryUserRepository(users);
+    await userRepo.add({ name, email });
+    const exists = await userRepo.exists({ name, email });
+    expect(exists).toBe(true);
+  })
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = [];
+    const name = 'any_name';
+    const email = 'any@gmail.com';
+    const userRepo = new InMemoryUserRepository(users);
+    await userRepo.add({ name, email });
+    const user = await userRepo.findUserByEmail(email);
+    expect(user.name).toBe('any_name');
+  })
 })
